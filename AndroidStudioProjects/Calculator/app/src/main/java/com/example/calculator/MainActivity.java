9cef90f8c184728d7d5b5fc31c66bdd1eeed7014
Saad GitHub <mirzasaadbaig865@gmail.com>
2023-07-02 23:40:31 +0530

    This iS The Final Commit

--- AndroidStudioProjects/Calculator/app/src/main/java/com/example/calculator/MainActivity.java
+++ AndroidStudioProjects/Calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -32,11 +33,12 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         decimalFormat = new DecimalFormat("#.##########");
         inputDisplay = findViewById(R.id.input);
---
+++ AndroidStudioProjects/Calculator/app/src/main/java/com/example/calculator/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+
+    public static int SPLASH_TIMER = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splach_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_TIMER);
+
+
+
+    }
+}
